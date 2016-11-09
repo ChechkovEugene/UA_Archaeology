33069802e6cbb242aa5971a39a65b0ba48046136
ChechkovEugene <Chechkov.Eugene@gmail.com>
2016-11-09 15:54:31 +0200

    Add login form

--- web/controllers/session_controller.ex
+++ web/controllers/session_controller.ex
@@ -1,7 +1,9 @@
 defmodule UaArchaeology.SessionController do
   use UaArchaeology.Web, :controller
 
+  alias UaArchaeology.User
+
   def new(conn, _params) do
-    render conn, "new.html"
+    render conn, "new.html", changeset: User.changeset(%User{})
   end
 end
