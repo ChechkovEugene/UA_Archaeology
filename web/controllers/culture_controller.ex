fcd6dd3e309cd676ad02aebba872e35f01df9883
ChechkovEugene <Chechkov.Eugene@gmail.com>
2016-11-10 17:23:48 +0200

    Add user linking for cultures

--- web/controllers/culture_controller.ex
+++ web/controllers/culture_controller.ex
@@ -7,16 +7,23 @@ defmodule UaArchaeology.CultureController do
 
   def index(conn, _params) do
     cultures = Repo.all(Culture)
+    # cultures = Repo.all(assoc(conn.assigns[:user], :cultures))
     render(conn, "index.html", cultures: cultures)
   end
 
   def new(conn, _params) do
-    changeset = Culture.changeset(%Culture{})
+    changeset =
+      conn.assigns[:user]
+      |> build_assoc(:cultures)
+      |> Culture.changeset()
     render(conn, "new.html", changeset: changeset)
   end
 
   def create(conn, %{"culture" => culture_params}) do
-    changeset = Culture.changeset(%Culture{}, culture_params)
+    changeset =
+      conn.assigns[:user]
+      |> build_assoc(:cultures)
+      |> Culture.changeset(culture_params)
 
     case Repo.insert(changeset) do
       {:ok, _culture} ->
@@ -30,17 +37,18 @@ defmodule UaArchaeology.CultureController do
 
   def show(conn, %{"id" => id}) do
     culture = Repo.get!(Culture, id)
+    # culture = Repo.get!(assoc(conn.assigns[:user], :cultures), id)
     render(conn, "show.html", culture: culture)
   end
 
   def edit(conn, %{"id" => id}) do
-    culture = Repo.get!(Culture, id)
+    culture = Repo.get!(assoc(conn.assigns[:user], :cultures), id)
     changeset = Culture.changeset(culture)
     render(conn, "edit.html", culture: culture, changeset: changeset)
   end
 
   def update(conn, %{"id" => id, "culture" => culture_params}) do
-    culture = Repo.get!(Culture, id)
+    culture = Repo.get!(assoc(conn.assigns[:user], :cultures), id)
     changeset = Culture.changeset(culture, culture_params)
 
     case Repo.update(changeset) do
@@ -54,7 +62,7 @@ defmodule UaArchaeology.CultureController do
   end
 
   def delete(conn, %{"id" => id}) do
-    culture = Repo.get!(Culture, id)
+    culture = Repo.get!(assoc(conn.assings[:user], :cultures), id)
 
     # Here we use delete! (with a bang) because we expect
     # it to always work (and if it does not, it will raise).
@@ -68,10 +76,18 @@ defmodule UaArchaeology.CultureController do
   defp assign_user(conn, _opts) do
     case conn.params do
       %{"user_id" => user_id} ->
-        user = Repo.get(UaArchaeology.User, user_id)
-        assign(conn, :user, user)
-      _->
-        conn
+        case Repo.get(UaArchaeology.User, user_id) do
+          nil  -> invalid_user(conn)
+          user -> assign(conn, :user, user)
+        end
+      _ -> invalid_user(conn)
     end
+  end
+
+  defp invalid_user(conn) do
+    conn
+    |> put_flash(:error, "Невірний користувач!")
+    |> redirect(to: page_path(conn, :index))
+    |> halt
   end
 end
